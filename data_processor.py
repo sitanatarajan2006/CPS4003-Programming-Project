6be74179e6417b8d79b6f564dbb754862ebedbe8
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-01-01 16:40:35 +0000

    added find video function

--- data_processor.py
+++ data_processor.py
@@ -12,3 +12,12 @@ def unique_categories(data):
         else:
             category_count[category] = 1
     return category_count
+
+def find_video(data, search_value):
+    # Find one video by video_id or title
+
+    for row in data:
+        if row["video_id"] == search_value or row["title"] == search_value:
+            return row
+
+    return None
