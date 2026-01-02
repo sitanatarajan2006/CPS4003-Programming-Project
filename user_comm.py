fc0cff697a9ee77d8bfe7fdb92d90649ae2128ec
sitanatarajan2006 <sitanatarajan3@gmail.com>
2026-01-02 23:15:19 +0000

    added a home button to the menu with return to home function

--- user_comm.py
+++ user_comm.py
@@ -18,6 +18,7 @@ def start_gui(data):
 
     menu_area = tk.Frame(left_frame, width=500, bg="#3776a1")
     menu_area.pack(fill=tk.Y, expand=True, padx=5, pady=5)
+    menu_area.pack_propagate(False)
 
     content_area = tk.Frame(right_frame, bg="#5293bb")
     content_area.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
@@ -26,4 +27,17 @@ def start_gui(data):
     content_label.pack(pady=20, padx=20, anchor="nw")
 
 
+
+    def clear_content():
+        for widget in content_area.winfo_children():
+            widget.destroy()
+    
+    
+    def home():
+        clear_content()
+        home_label = tk.Label(content_area, text="Select an option from the menu", bg="#89cff1", fg="#003a6b", font=("Courier New", 16, "bold"), padx=20, pady=20, wraplength=900, justify="left")
+        home_label.pack(pady=20, padx=20, anchor="nw")
+    home_button = tk.Button(menu_area, text="Home", command=home, bg="#89cff1", fg="#003a6b", font=("Courier New", 14, "bold"), width=25, height=2, relief="flat")
+    home_button.pack(pady=20, padx=20)
+    
     app.mainloop()
